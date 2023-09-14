70eb0aba2d613dcd7c218c2353c60e1ff73745ba
julisingh011 <julisinghrajput1118@gmail.com>
2023-09-14 11:56:40 +0530

    adding the file

---
+++ file1.python.py
@@ -0,0 +1,17 @@
+'''print("enter first_number")
+user_input1 =int(input("any integer:"))
+if (user_input1%2)==0 :
+ print("the enterd number is even")
+else:
+ print("the enterd number is odd")'''
+
+'''for i in range(0,100000):
+ print("WELCOME TO Y WORLD")'''
+
+#for column
+for i in range(0,10):
+ #for column
+ for j in range(i+1):
+  print("*" , end= " ")
+print()
+ 
